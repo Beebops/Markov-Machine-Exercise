cc22b558c3ad125db933e967b91e389ade2d64f5
Beth Carretta <bethcarretta@gmail.com>
2023-07-14 12:16:17 -0400

    added tests

--- markov.test.js
+++ markov.test.js
@@ -1,13 +1,32 @@
 const { MarkovMachine } = require('./markov')
 
-// describe('add function', function () {
-//   test('return sum', function () {
-//     let sum = add(2, 3)
-//     expect(sum).toEqual(5)
-//   })
+describe('Markov Machine', () => {
+  describe('makeChains', () => {
+    it('should set the markov chains correctly', () => {
+      const text = 'the wind in my hair'
+      const markovMachine = new MarkovMachine(text)
+      const expectedChains = new Map([
+        ['the', ['wind']],
+        ['wind', ['in']],
+        ['in', ['my']],
+        ['my', ['hair']],
+        ['hair', [null]],
+      ])
+      expect(markovMachine.chains).toEqual(expectedChains)
+    })
+  })
 
-//   test('return sum with neg numbers', function () {
-//     let sum = add(-2, 3)
-//     expect(sum).toEqual(1)
-//   })
-// })
+  describe('getWord', () => {
+    it('should return a random word', () => {
+      const arr = ['hello', 'I', 'love', 'you']
+      const randomWord = MarkovMachine.getWord(arr)
+      expect(arr).toContain(randomWord)
+    })
+
+    it('should return null if array is empty', () => {
+      const arr = []
+      const randomWord = MarkovMachine.getWord(arr)
+      expect(randomWord).toBeNull()
+    })
+  })
+})
